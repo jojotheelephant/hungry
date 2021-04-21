625ebaf607fb2cbdecfd8d3d92185b9839549c11
jojotheelephant <33424322+jojotheelephant@users.noreply.github.com>
2021-04-21 10:05:33 -0700

    changed the uppercase H to lowercase h in title

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <h1>Hungry</h1>
+                <h1>hungry</h1>
                 <SearchBar searchYelp={this.searchYelp} />
                 <BusinessList businesses={this.state.businesses} />
             </div>
