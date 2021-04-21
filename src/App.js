66e45574c08d3578e9854edf0ef4c085682d18f1
jojotheelephant <33424322+jojotheelephant@users.noreply.github.com>
2021-04-21 08:37:55 -0700

    completed project. first commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BusinessList from "./Components/BusinessList";
+import SearchBar from "./Components/SearchBar";
+import Yelp from "./util/yelp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            businesses: [],
+        };
+
+        this.searchYelp = this.searchYelp.bind(this);
+    }
+
+    //runs ascyn request 'search' from Yelp.js then updates business state
+    searchYelp = (term, location, sortBy) => {
+        Yelp.search(term, location, sortBy).then((businesses) => {
+            this.setState({ businesses: businesses });
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hungry</h1>
+                <SearchBar searchYelp={this.searchYelp} />
+                <BusinessList businesses={this.state.businesses} />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/util/yelp.js
@@ -0,0 +1,40 @@
+const apiKey =
+    "Mu0hh-1a4vC7VhodH8jmQ0r5fdnIjR1ADm72FI_hVKv9QoYGvFw_O35xHPX54bSAv7hlu70I4H7r98H69ba2-J7nhJDhnSCKjRe3udeHNEO88Carh4xn5ObfEKF9YHYx";
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(
+            `https://thingproxy.freeboard.io/fetch/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${apiKey}`,
+                },
+            }
+        )
+            .then((response) => {
+                return response.json();
+            })
+            .then((jsonResponse) => {
+                if (jsonResponse.businesses) {
+                    console.log(jsonResponse.businesses);
+                    return jsonResponse.businesses.map((business) => {
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories.title,
+                            rating: business.rating,
+                            reviewCount: business.review_count,
+                            url: business.url,
+                        };
+                    });
+                }
+            });
+    },
+};
+
+export default Yelp;
